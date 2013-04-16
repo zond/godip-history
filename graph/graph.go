44787e4be13e6e1796cafcd14ac0637519c26f91
Martin Bruse <zondolfin@gmail.com>
2013-04-16 11:28:12 +0200

    ...

--- graph/graph.go
+++ graph/graph.go
@@ -1,6 +1,7 @@
 package graph
 
 import (
+  "bytes"
   "fmt"
 )
 
@@ -12,6 +13,14 @@ func New() *Graph {
 
 type Graph struct {
   nodes map[string]*Node
+}
+
+func (self *Graph) String() string {
+  buf := new(bytes.Buffer)
+  for _, n := range self.nodes {
+    fmt.Fprintf(buf, "%v", n)
+  }
+  return string(buf.Bytes())
 }
 
 func (self *Graph) Node(n string) *Node {
@@ -31,6 +40,15 @@ type Node struct {
   graph *Graph
 }
 
+func (self *Node) String() string {
+  buf := new(bytes.Buffer)
+  fmt.Fprintf(buf, "%v\n", self.name)
+  for _, s := range self.subs {
+    fmt.Fprintf(buf, "  %v\n", s)
+  }
+  return string(buf.Bytes())
+}
+
 func (self *Node) Sub(s string) *SubNode {
   if self.subs[s] == nil {
     self.subs[s] = &SubNode{
@@ -42,6 +60,16 @@ func (self *Node) Sub(s string) *SubNode {
   return self.subs[s]
 }
 
+func (self *Node) Conn(n, s string) *SubNode {
+  sub := self.Sub("")
+  return sub.Conn(n, s)
+}
+
+func (self *Node) Con(n string) *SubNode {
+  sub := self.Sub("")
+  return sub.Con(n)
+}
+
 type SubNode struct {
   name  string
   edges map[string]*SubNode
@@ -49,7 +77,21 @@ type SubNode struct {
   flags int
 }
 
-func (self SubNode) getName() string {
+func (self *SubNode) String() string {
+  buf := new(bytes.Buffer)
+  fmt.Fprintf(buf, "%v => ", self.name)
+  dests := make([]string, 0, len(self.edges))
+  for n, _ := range self.edges {
+    dests = append(dests, n)
+  }
+  fmt.Fprintf(buf, "%v", dests)
+  return string(buf.Bytes())
+}
+
+func (self *SubNode) getName() string {
+  if self.name == "" {
+    return fmt.Sprintf("%v", self.node.name)
+  }
   return fmt.Sprintf("%v/%v", self.node.name, self.name)
 }
 
@@ -59,6 +101,17 @@ func (self *SubNode) Conn(n, s string) *SubNode {
   return self
 }
 
-func (self SubNode) Done() *Graph {
+func (self *SubNode) Con(n string) *SubNode {
+  target := self.node.graph.Node(n).Sub("")
+  self.edges[target.getName()] = target
+  return self
+}
+
+func (self *SubNode) Done() *Graph {
   return self.node.graph
+}
+
+func (self *SubNode) Flag(flags int) *SubNode {
+  self.flags |= flags
+  return self
 }
