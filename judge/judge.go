92078f27cd08bd285ade0d8c32da2aa14ab19e79
Martin Bruse <zondolfin@gmail.com>
2013-04-19 10:54:55 +0200

    docu

--- judge/judge.go
+++ judge/judge.go
@@ -59,19 +59,22 @@ func (self *State) Next() *State {
   return self
 }
 
+/*
+Remember to clean up self.visited after calling this for a top level order!
+*/
 func (self *State) Resolve(prov Province) (result bool) {
-  if result, ok := self.guesses[prov]; !ok {
-    if self.visited[prov] {
+  if result, ok := self.guesses[prov]; !ok { // Already guessed
+    if self.visited[prov] { // Not yet guessed, but visited before, introduce a guess (the default false result) and return it.
       self.guesses[prov] = result
-    } else {
+    } else { // Not yet visited, do a proper adjudication.
       self.visited[prov] = true
 
-      result = self.Orders[prov].Adjudicate(self)
-      if _, ok := self.guesses[prov]; ok {
-        self.guesses[prov] = true
-        second_result := self.Orders[prov].Adjudicate(self)
-        if result != second_result {
-          result = self.BackupRule(self, prov, self.visited)
+      result = self.Orders[prov].Adjudicate(self) // Ask order to adjudicate itself.
+      if _, ok := self.guesses[prov]; ok {        // We were visited again, and depend on our guess.
+        self.guesses[prov] = true                           // Switch the guess to true.
+        second_result := self.Orders[prov].Adjudicate(self) // Ask order to adjudicate itself with the new guess.
+        if result != second_result {                        // If the results are the same, it means that exactly one of them were consistent (and any one of them could be returned). If not, none or both are consistent.
+          result = self.BackupRule(self, prov, self.visited) // So, run the BackupRule on the orders we visited and let it decide.
         }
       }
     }
