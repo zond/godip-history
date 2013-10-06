43a5ebb9e18300f57dc9c63a16818c8d8cb3ca0d
Martin Bruse <zondolfin@gmail.com>
2013-10-06 13:35:13 +0200

    added classica.orders.Parse that parses a string slice to order - using the same format as state.State#Options produces

--- classical/orders/orders.go
+++ classical/orders/orders.go
@@ -1,8 +1,10 @@
 package orders
 
 import (
+	"fmt"
 	cla "github.com/zond/godip/classical/common"
 	dip "github.com/zond/godip/common"
+	"time"
 )
 
 func Types() []dip.Order {
@@ -19,4 +21,43 @@ func Types() []dip.Order {
 		},
 		&support{},
 	}
+}
+
+func Parse(bits []string) (result dip.Order, err error) {
+	if len(bits) > 1 {
+		switch dip.OrderType(bits[1]) {
+		case (&build{}).DisplayType():
+			if len(bits) == 3 {
+				result = Build(dip.Province(bits[0]), dip.UnitType(bits[2]), time.Now())
+			}
+		case (&convoy{}).DisplayType():
+			if len(bits) == 4 {
+				result = Convoy(dip.Province(bits[0]), dip.Province(bits[2]), dip.Province(bits[3]))
+			}
+		case (&disband{}).DisplayType():
+			if len(bits) == 2 {
+				result = Disband(dip.Province(bits[0]), time.Now())
+			}
+		case (&hold{}).DisplayType():
+			if len(bits) == 2 {
+				result = Hold(dip.Province(bits[0]))
+			}
+		case (&move{}).DisplayType():
+			if len(bits) == 3 {
+				result = Move(dip.Province(bits[0]), dip.Province(bits[2]))
+			}
+		case (&move{flags: map[dip.Flag]bool{cla.ViaConvoy: true}}).DisplayType():
+			if len(bits) == 3 {
+				result = Move(dip.Province(bits[0]), dip.Province(bits[2])).ViaConvoy()
+			}
+		case (&support{}).DisplayType():
+			if len(bits) == 4 {
+				result = Support(dip.Province(bits[0]), dip.Province(bits[2]), dip.Province(bits[3]))
+			}
+		}
+	}
+	if result == nil {
+		err = fmt.Errorf("Invalid order %+v", bits)
+	}
+	return
 }
