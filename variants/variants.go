9b7a619f2e82093fa0adef0d00fc1193ca42ff71
Martin Bruse <zondolfin@gmail.com>
2014-12-19 10:54:28 +0100

    added a package to keep track of variants, and added the possibly simplest variant of them all: fleet rome

---
+++ variants/variants.go
@@ -0,0 +1,66 @@
+package variants
+
+import (
+	"github.com/zond/godip/classical"
+	cla "github.com/zond/godip/classical/common"
+	"github.com/zond/godip/classical/orders"
+	dip "github.com/zond/godip/common"
+	"github.com/zond/godip/state"
+)
+
+const (
+	Classical = "classical"
+	FleetRome = "fleetrome"
+)
+
+type Variant struct {
+	Start       func() (*state.State, error)
+	Blank       func(dip.Phase) *state.State
+	Phase       func(int, dip.Season, dip.PhaseType) dip.Phase
+	Nations     func() []dip.Nation
+	PhaseTypes  func() []dip.PhaseType
+	Seasons     func() []dip.Season
+	UnitTypes   func() []dip.UnitType
+	Orders      func() []dip.Order
+	ParseOrders func(map[dip.Nation]map[dip.Province][]string) (map[dip.Province]dip.Adjudicator, error)
+	ParseOrder  func([]string) (dip.Adjudicator, error)
+}
+
+var Variants = map[string]Variant{
+	Classical: Variant{
+		Start:       classical.Start,
+		Blank:       classical.Blank,
+		Phase:       classical.Phase,
+		Orders:      orders.Orders,
+		ParseOrders: orders.ParseAll,
+		ParseOrder:  orders.Parse,
+		Nations:     func() []dip.Nation { return cla.Nations },
+		PhaseTypes:  func() []dip.PhaseType { return cla.PhaseTypes },
+		Seasons:     func() []dip.Season { return cla.Seasons },
+		UnitTypes:   func() []dip.UnitType { return cla.UnitTypes },
+	},
+	FleetRome: Variant{
+		Start: func() (result *state.State, err error) {
+			if result, err = classical.Start(); err != nil {
+				return
+			}
+			result.RemoveUnit(dip.Province("rom"))
+			if err = result.SetUnit(dip.Province("rom"), dip.Unit{
+				Type:   cla.Fleet,
+				Nation: cla.Italy,
+			}); err != nil {
+				return
+			}
+			return
+		},
+		Blank:       classical.Blank,
+		Phase:       classical.Phase,
+		Orders:      orders.Orders,
+		ParseOrders: orders.ParseAll,
+		ParseOrder:  orders.Parse,
+		Nations:     func() []dip.Nation { return cla.Nations },
+		PhaseTypes:  func() []dip.PhaseType { return cla.PhaseTypes },
+		Seasons:     func() []dip.Season { return cla.Seasons },
+		UnitTypes:   func() []dip.UnitType { return cla.UnitTypes },
+	},
+}
